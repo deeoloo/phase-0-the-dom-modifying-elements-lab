cdea9028c79987374236ed7d5c5879ded6c8d7dd
deenoloo <doreenoloo2@gmail.com>
2025-02-27 06:29:07 -0500

    the-dom-modifying-elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const element = document.getElementsByTagName("main");
+main.remove("main");
+
+const newHeader = document.createElement("h1");
+newHeader.id = ("victory");
+
+newHeader.textContent = ("Dorine is the champion");
+
+
+
